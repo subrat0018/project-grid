54715e00cc5261066200e96b7f259a028961dc14
subrat0018 <subratchandra2003@gmail.com>
2023-08-18 18:27:47 +0530

    feat: signup, userGetter 👍

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,32 +1,51 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../database/models/user");
-const passport = require("../passport");
 //This function handles & serves the signup requests
 //Creates a new user using the Database' User Schema and stores it 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   console.log("user signup");
 
-  const { name, email, password } = req.body;
+  const { name, email, userType, walletAddress} = req.body;
   // ADD VALIDATION
-  User.findOne({ email:email}).then((err, user) => {
-    if (err) {
-      console.log("User.js post error: ", err);
-    } else if (user) {
-      res.json({
-        error: `Sorry, already a user with the username: ${empID}`,
-      });
-    } else {
-      const newUser = new User({
-        name: name,
-        email: email,
-        password: password,
-      });
-      newUser.save().then((err, savedUser) => {
-        if (err) return res.json(err);
-        res.json(savedUser);
-      });
+  const user = await User.findOne({walletAddress: walletAddress})
+  if(user)
+  {
+    res.json({
+      error: `Sorry, already a user with the walletAddress: ${walletAddress}`,
+    });
+  }
+  else
+  {
+    const newUser = new User({
+      name: name,
+      email: email,
+      userType: userType,
+      walletAddress: walletAddress
+    });
+    newUser.save().then((err, savedUser) => {
+      if (err) return res.json(err);
+      res.json(savedUser);
+    });
+  }
+});
+router.post("/getdetails",async (req,res)=>{
+  const {walletAddress} = req.body
+  const user = await User.findOne({walletAddress: walletAddress});
+  res.send(user);
+})
+router.get("/getsellers", async (req, res)=>{
+  const users = await User.find();
+  sellers = new Array();
+  users.forEach(user => {
+    if(user.userType === "Seller")
+    {
+      sellers.push(user);
     }
   });
+  res.send(sellers);
 });
+
+
+
 module.exports = router
