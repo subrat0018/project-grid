489589a8df260f9df0aa901139c5b4b4fd087c46
subrat0018 <subratchandra2003@gmail.com>
2023-08-20 16:27:25 +0530

    feat: Coin History in Dashboard

--- client/src/pages/dashboard/CustomerProfile.jsx
+++ client/src/pages/dashboard/CustomerProfile.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import Web3Context from '../../contexts';
+import { balanceOf } from '../../contexts/useContract/readContract';
 const CustomerProfile = (props) => {
+  const {Contract, account} = useContext(Web3Context);
+  const [balance, setBalance] = useState(0);
+  useEffect(()=>{
+    balanceOf(Contract, account.currentAccount).then(res=>setBalance(res));
+  },[account]);
   return (
     <>
       <div class="flex w-full flex-col items-center justify-center rounded-lg">
@@ -13,10 +20,9 @@ const CustomerProfile = (props) => {
           <h5 class="font-gotu text-3xl font-semibold tracking-tight text-gray-900">
             {props.Name}
           </h5>
-          <div className="flex items-center justify-between mt-2 mx-2">
-            <p className="font-gotu text-base font-bold">30 orders</p>
+          <div className=" w-full flex items-center justify-center mt-2 mx-2">
             <span className="flex items-center justify-center font-gotu text-base font-bold">
-              {`60 `}
+              {balance}
               <img
                 src="https://res.cloudinary.com/sambitsankalp/image/upload/v1692195660/Bitcoin_Cash_cpb1xm.png"
                 alt="BD"
