c0a88f5a6d38fdb502a0d398ab3a2054fbec3db0
subrat0018 <subratchandra2003@gmail.com>
2023-08-21 02:43:03 +0530

    fix: Added tokenomics

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -5,6 +5,7 @@ const Table = () => {
   return (<>
 
 <div class="relative overflow-x-auto">
+<h1>Tokenomics</h1>
     <h1 className="mb-2">Base Supply - 10 million coins*</h1>
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
